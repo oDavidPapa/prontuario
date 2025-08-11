2cdbb48e0f9bfdc94c7a1825e85768de577284af
David Papa <davids.papa@outlook.com>
2025-08-11 13:49:47 -0300

    Cors Definitions

--- src/main/java/com/ufes/prontuario/config/security/jwt/WebConfig.java
+++ src/main/java/com/ufes/prontuario/config/security/jwt/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://prontuario-web.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
